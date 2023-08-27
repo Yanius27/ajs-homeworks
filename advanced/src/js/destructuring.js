14b15017e8470ad6fc06c6b2a3600b369ad3a70b
Yanius27 <zhzh-1987@mail.ru>
2023-08-27 22:47:44 +0700

    work on mistakes~

--- advanced/src/js/destructuring.js
+++ advanced/src/js/destructuring.js
@@ -1,12 +1,8 @@
 export default function destructuring(obj) {
   const arr = [];
   obj.special.forEach((el) => {
-    if (!('description' in el)) {
-      // eslint-disable-next-line no-param-reassign
-      el.description = 'Описание недоступно';
-    }
     const {
-      id, name, icon, description,
+      id, name, icon, description = 'Описание недоступно',
     } = el;
     arr.push({
       id, name, icon, description,
--- modules/ESM/src/js/game.js
+++ modules/ESM/src/js/game.js
@@ -3,6 +3,7 @@ import Character from "./domain.js";
 export default class Game {
   start() {
     console.log('game started');
+    console.log(global);
   }
 };
 
